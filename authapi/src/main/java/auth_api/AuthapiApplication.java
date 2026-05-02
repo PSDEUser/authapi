1748e3a386de8481aaf214e80c9f0933410bb320
PSDEUser <privatesoftwaredeveloperemail@gmail.com>
2026-05-02 07:01:07 +0200

    FIRST COMMIT; authapi prototype base created.

---
+++ authapi/src/main/java/auth_api/AuthapiApplication.java
@@ -0,0 +1,13 @@
+package auth_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthapiApplication.class, args);
+	}
+
+}
