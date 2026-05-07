b4868a8bda4c8864bd879da395e93b088b31aee1
PSDEUser <privatesoftwaredeveloperemail@gmail.com>
2026-05-07 15:02:59 +0200

    v0.0.4: Real User Registration, BCrypt password config. Added ApplicationConfig, DTO RegisterRequest created, AuthService created, AuthController created. Endpoint tried.

---
+++ authapi/src/main/java/auth_api/service/AuthService.java
@@ -0,0 +1,39 @@
+package auth_api.service;
+
+import auth_api.dto.RegisterRequest;
+import auth_api.model.Role;
+import auth_api.model.User;
+import auth_api.repository.RoleRepository;
+import auth_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void register(RegisterRequest request) {
+
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        Role userRole = roleRepository.findByName("USER")
+                .orElseThrow(() -> new RuntimeException("Role USER not found"));
+
+        User user = User.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .roles(Set.of(userRole))
+                .build();
+
+        userRepository.save(user);
+    }
+}
